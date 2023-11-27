953bfde78ed468a2383ec83a3a9a87e9d05cc515
Jamie Cassidy <jamiec90@gmail.com>
2023-11-27 13:35:37 +0000

    Remove what's checked link

--- src/_assets/scripts/OnlineWorkApproval/Components/VehicleCard/index.jsx
+++ src/_assets/scripts/OnlineWorkApproval/Components/VehicleCard/index.jsx
@@ -7,7 +7,7 @@ const VehicleCard = props => (
     <span className="ch-fs--4 ch-text--bold ch-display--block">{props.vehicle.manufacturer} {props.vehicle.model} {props.vehicle.spec}</span>
     <div className="ch-mt--2">
       <span className="ch-chip ch-chip--lg ch-chip--yellow ch-mr--2 ch-fw--600">{props.vehicle.reg}</span>
-      <a href="#">What's checked?</a>
+      {/* <a href="#">What's checked?</a> */}
     </div>
   </Card>
 );
