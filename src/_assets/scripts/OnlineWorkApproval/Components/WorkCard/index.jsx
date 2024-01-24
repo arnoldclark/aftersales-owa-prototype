f71c2b0f52f3e26c0f4b635f346f135ad0887a72
Jamie Cassidy <jamiec90@gmail.com>
2024-01-24 14:45:09 +0000

    Update multiline part UI to be clearer

--- src/_assets/scripts/OnlineWorkApproval/Components/WorkCard/index.jsx
+++ src/_assets/scripts/OnlineWorkApproval/Components/WorkCard/index.jsx
@@ -18,22 +18,34 @@ const WorkCard = props => {
     </>
   )
   return (
-    <Card className={props.className}>
-      <div className="ch-display--flex ch-align-items--center ch-justify-content--between ch-mb--2">
-        { props.type == "essential" ? <Essential /> : <Advisory />}
-      </div>
+    <div className={`ch-ba--1 ch-bc--grey-3 ch-bg--white ch-rounded ch-shadow--md ${props.className}`}>
+      <div className="ch-pa--3">
+        <div className="ch-display--flex ch-align-items--center ch-justify-content--between ch-mb--2">
+          { props.type == "essential" ? <Essential /> : <Advisory />}
+        </div>
 
-      <div>
-        <span className="ch-fs--1 ch-text--uppercase ch-fw--600 ch-color--grey-5 ch-display--block">{props.group}</span>
-        <h2 className="ch-fs--4 ch-mb--1">{props.subGroup} - {props.component}</h2>
-        <span className="ch-fs--3">{props.cause} - {props.action}</span>
+        <div>
+          <span className="ch-fs--1 ch-text--uppercase ch-fw--600 ch-color--grey-5 ch-display--block">{props.group}</span>
+          <h2 className="ch-fs--4 ch-mb--1">{props.subGroup} - {props.component}</h2>
+          <span className="ch-fs--3">{props.cause} - {props.action}</span>
+        </div>
+
+        { props.notes && (
+          <div className="ch-rounded ch-bg--grey-1 ch-pa--2 ch-ba--1 ch-bc--grey-2 ch-mt--2">
+            <h3 className="ch-fs--3 ch-mb--1">Technician's notes</h3>
+            <p className="ch-mb--0 ch-fs--3">{props.notes}</p>
+          </div>
+        )}
       </div>
 
       {props.partDescriptions.length > 0 && (
-        <div className="ch-ba--1 ch-bc--grey-3 ch-pa--2 ch-bg--white ch-rounded ch-mt--2">
-          <div className="ch-display--flex ch-justify-content--between ch-align-items--center">
-            <span className="ch-fw--500 ch-fs--2">Includes multiple parts</span>
-            <button className="ch-btn ch-btn--link ch-pa--0" onClick={() => setShowParts(!showParts)}>{ showParts ? "Hide parts" : "Show parts" }</button>
+        <div className="ch-bt--1 ch-bc--grey-3 ch-bg--grey-1 ch-pa--3 owa-multipart" onClick={() => setShowParts(!showParts)}>
+          <div className="ch-display--flex ch-align-items--center ch-justify-content--between">
+            <div className="ch-display--flex">
+              <img src="/images/icon-attention.svg" className="ch-mr--2" alt="Attention" />
+              <span className="ch-fs--3">This repair includes multiple parts</span>
+            </div>
+            <button className="ch-btn ch-btn--link ch-pa--0 ch-display--block ch-ml--4" style={{whiteSpace: "nowrap"}} onClick={() => setShowParts(!showParts)}>{ showParts ? "Hide parts" : "Show parts" }</button>
           </div>
           { showParts &&
             <ul className="ch-mt--2">
@@ -46,14 +58,7 @@ const WorkCard = props => {
           }
         </div>
       )}
-
-      { props.notes && (
-        <div className="ch-rounded ch-bg--grey-1 ch-pa--3 ch-ba--1 ch-bc--grey-2">
-          <h3 className="ch-fs--3 ch-mt--2 ch-mb--1">Technician's notes</h3>
-          <p className="ch-mb--0 ch-fs--3">{props.notes}</p>
-        </div>
-      )}
-    </Card>
+    </div>
   )
 };
 
